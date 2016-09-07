8cf9f8db279e87fcf3315bb29e5467cdedb95687
supermancbs <clark.sacktor@gmail.com>
2016-09-07 17:33:26 -0400

    finished rails/sinatra section

--- rails-sinatra/users_controller.rb
+++ rails-sinatra/users_controller.rb
@@ -6,18 +6,53 @@ class UsersController < ApplicationController
   # /users/new
 
   # create
+  def create
+    @user = User.create(user_params)
+  end
+  # post to /users
 
   # index
+  def index
+    @users = User.all
+  end
+  # get to /users
 
   # show
+  def show
+    @user = User.find(params[:id])
+  end
+  # get to /users/:id
 
   # edit
+  def edit
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+  # get /user/:id/edit
 
   # update
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+  # patch to /user/:id/
+
   # destroy
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+  # delete to /users/:id
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :hometown)
+    end
 
 end
+
+
 
 
 # Write the controller action as if this was a rails controller
