9b44441a7ae78a01d05bd4ed7f5ee96d8c5296b9
supermancbs <clark.sacktor@gmail.com>
2016-09-07 14:05:32 -0400

    first commit

---
+++ object_relations/notes.rb
@@ -0,0 +1,21 @@
+# We want to add the following methods to each of the classes
+# An all method, where each object is added upon initialization of the object
+# A find_by_name method for both the customer and the restaurant
+# Build the following methods on the customer class
+  # Customer#add_review
+  # Customer#add_restaurant
+
+  # Review#customer
+  # Review#restaurant
+
+  # Restaurant#customers
+  # Restaurant#reviews
+
+# If you are not re-sketching out your domain, and thinking about single source of truth,
+# you are doing it wrong.
+
+#Customer has many reviews
+#Customer has many restaurants through reviews
+#A restaurant has many reviews
+#A restaurant has many customers through reviews
+#review belongs to customers and reviews 
