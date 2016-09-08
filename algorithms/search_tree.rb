14f35116a726f7fed51fab2b72dfce980237b836
supermancbs <clark.sacktor@gmail.com>
2016-09-07 21:21:34 -0400

    fixed algorithms section

--- algorithms/search_tree.rb
+++ algorithms/search_tree.rb
@@ -1,14 +1,41 @@
 # Given the root note in a sorted binary search tree, how do you find the smallest element
-# assumes class methods
 
-def search_true(node)
-  if node.left_child==nil && node.right_child==nil
-    return node.value
-  elsif node.left_child==nil
-    return node.value
-  elsif  node.left_child.value > node.left_child.value
-    search_true(left_child)
-  else
-    search_true(right_child)
+class Node
+  
+  attr_accessor :value, :left_child, :right_child
+
+  def initialize(value)
+    @value = value
   end
+
+  def search_tree
+    if self.left_child==nil || self.right_child==nil
+      self.value
+    else
+      self.left_child.search_tree
+    end
+  end
+
 end
+
+root_node = Node.new(9)
+
+node1 = Node.new(6)
+node2 = Node.new(10)
+node3 = Node.new(7)
+node4 = Node.new(3)
+node5 = Node.new(6)
+node6 = Node.new(15)
+node7 = Node.new(9)
+
+
+root_node.left_child = node1
+root_node.right_child = node2
+
+node1.left_child = node4
+node1.right_child = node5
+
+node2.left_child = node6
+node2.right_child = node7
+
+puts root_node.search_tree== 3
